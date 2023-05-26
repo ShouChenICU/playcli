14e310d82b20408865bc9e0fdc8494dd811ef0fa
不神秘的神秘人 <55085306+ShouChenICU@users.noreply.github.com>
2023-05-26 12:39:15 +0800

    Update playcli.sh

--- playcli.sh
+++ playcli.sh
@@ -15,22 +15,21 @@ then
 	exit -1
 fi
 
-echo 'Playing...'
 export COLORTERM=truecolor
 FILE=$1
-START_OFFSET=$([ $# -gt 1 ]  && echo -n $2 || echo -n 0)
-SPEED=$([ $# -gt 2 ]  && echo -n $3 || echo -n 1)
+START_OFFSET=`[ $# -gt 1 ]  && echo -n $2 || echo -n 0`
+SPEED=`[ $# -gt 2 ]  && echo -n $3 || echo -n 1`
 DURATION=`ffprobe -v quiet -show_format "$FILE" | grep duration | tr "=" " " | awk '{print $2}'`
 # FRAME_RATE=`ffprobe -v quiet -select_streams v -show_entries stream=r_frame_rate "$FILE" | grep r_frame_rate | tr "=" " " | awk '{print $2}'`
 # FRAME_RATE_NEW=`echo "scale=3; $SPEED * $FRAME_RATE" | bc`
 TIME_START=`date +%s.%N`
 TIME_START=`echo "scale=3; $TIME_START - $START_OFFSET / $SPEED" | bc`
-TIME_NOW=$TIME_START
 TIME_OFFSET=0
 WIDTH=`stty size|awk '{print $2}'`
 HEIGHT=`stty size|awk '{print $1}'`
 HEIGHT=$[$HEIGHT - 2]
 
+echo 'Playing...'
 while [ `echo "scale=3; $DURATION >= $TIME_OFFSET" | bc` -eq 1 ]
 do
 	ffmpeg -v quiet -ss `printf "%.3f" $TIME_OFFSET` -i "$FILE" -f apng -an -c:v apng -frames 1 - | jp2a --color --fill --chars="  " --size=$WIDTH"x"$HEIGHT -
@@ -38,5 +37,4 @@ do
 	TIME_NOW=`date +%s.%N`
 	TIME_OFFSET=`echo "scale=3; ($TIME_NOW - $TIME_START) * $SPEED" | bc`
 done
-
 echo 'Play done.'
